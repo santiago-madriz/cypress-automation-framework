209773961924f188f86f3efd3833ce6b6436023c
Santiago <santiagomadrizc@gmail.com>
2024-06-01 13:55:50 -0600

    initial

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+// cypress/e2e/sample_spec.cy.js
+describe('My Personal Site', () => {
+  it('should load the home page', () => {
+    cy.visit('https://santiagomadriz.com');
+    cy.contains('Santiago Madriz');
+    cy.get('[href="#work"]').click();
+    cy.url().should('include', '#work');
+    cy.contains('Konrad');
+  });
+});
