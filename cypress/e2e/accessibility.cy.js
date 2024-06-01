b5674bdc5a75402c75fd275f2e0ae49ed48059f2
Santiago <santiagomadrizc@gmail.com>
2024-06-01 14:22:17 -0600

    accessibility fixes

--- cypress/e2e/accessibility.cy.js
+++ cypress/e2e/accessibility.cy.js
@@ -1,5 +1,6 @@
 import 'cypress-axe';
 
+// cypress/e2e/accessibility_spec.cy.js
 describe('Accessibility Tests', () => {
     beforeEach(() => {
       cy.visit('https://santiagomadriz.com');
@@ -7,15 +8,35 @@ describe('Accessibility Tests', () => {
     });
   
     it('should check a11y violations on the home page', () => {
-      cy.checkA11y();
+      cy.checkA11y(null, null, (violations) => {
+        violations.forEach((violation) => {
+          const nodes = Cypress.$(violation.nodes.map((node) => node.target).join(','));
+          Cypress.log({
+            name: 'a11y error!',
+            consoleProps: () => violation,
+            $el: nodes,
+            message: `[${violation.help}](${violation.helpUrl})`
+          });
+        });
+      });
     });
   
     it('should check a11y violations on each section', () => {
       const sections = ['#expertise', '#about', '#projects', '#certifications', '#work', '#contact'];
-      sections.forEach(section => {
+      sections.forEach((section) => {
         cy.get(`[href="${section}"]`).click();
         cy.injectAxe();
-        cy.checkA11y(section);
+        cy.checkA11y(section, null, (violations) => {
+          violations.forEach((violation) => {
+            const nodes = Cypress.$(violation.nodes.map((node) => node.target).join(','));
+            Cypress.log({
+              name: 'a11y error!',
+              consoleProps: () => violation,
+              $el: nodes,
+              message: `[${violation.help}](${violation.helpUrl})`
+            });
+          });
+        });
       });
     });
   });
