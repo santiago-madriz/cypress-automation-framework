88ffcfd66c325869cccc00d6cccec8ed5a136e12
Santiago <santiagomadrizc@gmail.com>
2024-06-01 13:59:09 -0600

    accessibility

---
+++ cypress/e2e/accessibility.cy.js
@@ -0,0 +1,18 @@
+describe('Accessibility Tests', () => {
+    beforeEach(() => {
+      cy.visit('https://santiagomadriz.com');
+      cy.injectAxe();
+    });
+  
+    it('should check a11y violations on the home page', () => {
+      cy.checkA11y();
+    });
+  
+    it('should check a11y violations on each section', () => {
+      const sections = ['#expertise', '#about', '#projects', '#certifications', '#work', '#contact'];
+      sections.forEach(section => {
+        cy.get(`[href="${section}"]`).click();
+        cy.checkA11y(section);
+      });
+    });
+  });
